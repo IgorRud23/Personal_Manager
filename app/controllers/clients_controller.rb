4234d30f5b7df3b20d9be254ae8f3293fbe557f8
IgorRud23 <rud.igor23@gmail.com>
2019-08-11 23:12:05 -0400

    adited controllers

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,78 @@
 class ClientsController < ApplicationController
-  
+
+  def index
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+      @clients = User.find(params[:user_id]).clients
+    else
+      redirect_to user_clients_path
+    end
+  end
+
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      @user = current_user
+      redirect_to user_client_path(@user, @client)
+    else
+      render :new
+    end
+  end
+
+  def new
+    if params[:user_id] && !User.exists?(params[:user_id])
+      flash[:notice] = 'User not found.'
+      redirect_to new_user_registration_path
+    else
+      @client = Client.new(user_id: params[:user_id])
+    end
+  end
+
+  def show
+    @client = client
+    @user = current_user
+  end
+
+  def update
+    @client = client
+    @client.update(client_params)
+    if @client.errors.none?
+      @user = current_user
+      redirect_to user_client_path(@user, @client)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user.nil?
+        flash[:notice] = 'User not found.'
+        redirect_to new_user_registration_path
+      else
+        @client = user.clients.find_by(id: params[:id])
+        redirect_to user_clients_path(user), flash[:notice] = 'Client not found.' if @client.nil?
+      end
+    else
+      @client = client
+      @user = current_user
+    end
+  end
+
+  def destroy
+    client.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :gender, :age, :weight, :height, :user_id)
+  end
+
+  def client
+    @client ||= Client.find(params[:id])
+  end
+
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,78 @@
 class SchedulesController < ApplicationController
-end
+
+  def index
+    if params[:user_id]
+      @schedules = User.find(params[:user_id]).schedules
+      @user = User.find(params[:user_id])
+    else
+      redirect_to user_schedules_path
+    end
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      @user = current_user
+      redirect_to user_schedule_path(@user, @schedule)
+    else
+      render :new
+    end
+  end
+
+  def new
+    if params[:user_id] && !User.exists?(params[:user_id])
+      flash[:notice] = 'User not found.'
+      redirect_to new_user_registration_path
+    else
+      @schedule = Schedule.new(user_id: params[:user_id])
+    end
+  end
+
+  def update
+    @schedule = schedule
+    @schedule.update(schedule_params)
+    if @schedule.errors.none?
+      @user = current_user
+      redirect_to user_schedule_path(@user, @schedule)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user.nil?
+        redirect_to new_user_registration_path, flash[:notice] = 'User not found.'
+      else
+        @schedule = user.schedules.find_by(id: params[:id])
+        redirect_to user_schedules_path(user),
+        flash[:notice] = 'Schedule not found.' if @schedule.nil?
+      end
+    else
+      @schedule = schedule
+      @user = current_user
+    end
+  end
+
+  def show
+    @schedule = schedule
+    @user = current_user
+  end
+
+  def destroy
+    schedule.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:date_day, :date_time, :exercise_type, :user_id, :client_id, :gym_list_id)
+  end
+
+  def schedule
+    @schedule ||= Schedule.find(params[:id])
+  end
+
+  end
